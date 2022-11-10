0e703850caadacccf9ad5fa83dd376ae49e3a4ee
eyad-mansour <eyad.mansour.1997@gmail.com>
2022-11-10 16:59:05 +0300

    add oneReapeated function

--- javascript/code-challenges/hashtable-challenges/challenge02/challenge02.js
+++ javascript/code-challenges/hashtable-challenges/challenge02/challenge02.js
@@ -1,1 +1,16 @@
-// Write here the code challenge solution
+'use strict';
+
+function oneReapeated(str) {
+  let arr = str.split(' ');
+  let hash = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (hash[arr[i]]) {
+      return arr[i];
+    } else {
+      hash[arr[i]] = 1;
+    }
+  }
+  return 'no repeated word';
+}
+
+// console.log(oneReapeated('ASAC is a department at LTUC. ASAC teaches programming in LTUC'));
