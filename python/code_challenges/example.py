a68f29c02820b97593d3c6ba47826a447fc5993d
eyad-mansour <100406970+eyad-mansour@users.noreply.github.com>
2023-09-05 10:00:34 +0300

    Delete python directory

--- python/code_challenges/example.py
+++
@@ -1,4 +0,0 @@
-#  Testing pulling the code challenge:
-#  write here the code challenge instruction
-def sum(a, b):
-    return a+b
--- python/pull.py
+++
@@ -1,8 +0,0 @@
-# Don't Edit
-import subprocess
-import sys
-
-topic = sys.argv[1]
-number = sys.argv[2]
-
-subprocess.call(['bash', './bin/shell.sh', topic, number])
